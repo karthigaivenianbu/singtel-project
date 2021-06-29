b1b964ce50e48ac13408b9e646082fd50456d915
karthigaivenianbu <86611671+karthigaivenianbu@users.noreply.github.com>
2021-06-30 04:20:14 +0800

    Add files via upload

---
+++ TestRunner.java
@@ -0,0 +1,56 @@
+package com.example.ui.runner;
+
+import com.example.ui.helper.BaseWeb;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+@CucumberOptions(features = { "src/test/resources/features" }, glue = {
+		"com/example/ui/stepdefinitions" }, tags = "@MVCTest" ,plugin = {
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"json:target/cucumber-report.json" })
+
+public class TestRunner {
+
+	private TestNGCucumberRunner cucumberRunner;
+
+	@BeforeSuite
+	public void setupEnvironment() {
+	}
+
+	@BeforeClass(alwaysRun = true)
+	public void setUpClass() throws Exception {
+		cucumberRunner = new TestNGCucumberRunner(TestRunner.class);
+	}
+
+
+	@Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+	public void scenario(PickleWrapper pickle, FeatureWrapper cucumberFeature) throws Throwable {
+		cucumberRunner.runScenario(pickle.getPickle());
+	}
+
+	@AfterTest
+	public void tearDown() {
+		BaseWeb.closeBrowser();
+	}
+	@AfterMethod
+	public void cleanup(ITestResult result) {
+		BaseWeb.takeScreenshot(result.getName());
+
+	}
+
+	@DataProvider
+	public Object[][] scenarios() {
+		return cucumberRunner.provideScenarios();
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDownClass() throws Exception {
+		cucumberRunner.finish();
+	}
+
+
+}
