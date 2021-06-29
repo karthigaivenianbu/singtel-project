daf8050f2de44bd9d4a935e0e7d4f82df56ec192
karthigaivenianbu <86611671+karthigaivenianbu@users.noreply.github.com>
2021-06-30 03:40:54 +0800

    Add files via upload
    baseWeb

---
+++ BaseWeb.java
@@ -0,0 +1,71 @@
+package com.example.ui.helper;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class BaseWeb {
+    static WebDriver driver;
+    static String browserName,websiteURL;
+    static String URL=null;
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+
+            ConfigFileReader configFileReader = new ConfigFileReader();
+            browserName = configFileReader.getPropertyValues("browser");
+            websiteURL = configFileReader.getPropertyValues("url");
+             URL = "https://" + "public" + ":" + "Let$BeC001" + "@" + websiteURL;
+            if (browserName.contains("firefox") || browserName.contains("ff")) {
+                WebDriverManager.chromedriver().setup();
+                driver = new FirefoxDriver();
+            }
+            if (browserName.contains("chrome") || browserName.contains("cr")) {
+                WebDriverManager.chromedriver().setup();
+               driver = new ChromeDriver();
+                WebDriver driver = getDriver();
+
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+        driver.get(URL);
+        return driver;
+
+    }
+
+    public static void closeBrowser() {
+        if (driver != null)
+            driver.quit();
+    }
+
+
+    public static void takeScreenshot(String name) {
+        File screenshotdirectory = new File("test-output/Screenshots");
+
+        if (!screenshotdirectory.exists()) {
+            screenshotdirectory.mkdir();
+        }
+        try {
+            String fileName = screenshotdirectory + "/" + name + System.currentTimeMillis() + ".png";
+            TakesScreenshot ts = (TakesScreenshot) driver;
+
+            File source = ts.getScreenshotAs(OutputType.FILE);
+
+            FileUtils.copyFile(source, new File(fileName));
+
+            System.out.println("Screenshot taken - " + fileName);
+        } catch (Exception e) {
+
+            System.out.println("Exception while taking screenshot " + e.getMessage());
+        }
+    }
+
+}
